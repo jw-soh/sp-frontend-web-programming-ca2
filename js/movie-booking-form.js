724ff2e4d8b37eb2108875031fd6c984ecd7f76e
JW Soh <kylie@Kylies-MacBook-Air.local>
2021-07-22 13:31:14 +0800

    Beautify JS for movie booking form

--- js/movie-booking-form.js
+++ js/movie-booking-form.js
@@ -1,7 +1,9 @@
-window.onload = (event) => {
+window.onload = () => {
   const movieBookingButton = document.querySelector(' .navbar-buttons > .btn ');
+
   movieBookingButton.addEventListener('click', () => {
     const movieBookingForm = document.querySelector(' .movie-booking-form ');
+
     movieBookingForm.classList.toggle('disappear');
   })
 };
