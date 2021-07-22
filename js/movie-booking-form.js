fa8b76d5ee596e70f0b8c277f44a89a859c83db4
JW Soh <kylie@Kylies-MacBook-Air.local>
2021-07-22 13:27:38 +0800

    Add toggle on/off functionality for movie booking form

---
+++ js/movie-booking-form.js
@@ -0,0 +1,7 @@
+window.onload = (event) => {
+  const movieBookingButton = document.querySelector(' .navbar-buttons > .btn ');
+  movieBookingButton.addEventListener('click', () => {
+    const movieBookingForm = document.querySelector(' .movie-booking-form ');
+    movieBookingForm.classList.toggle('disappear');
+  })
+};
