161a2386554b4fec0984093f0c37dab6c6b631f8
JW Soh <kylie@Kylies-MacBook-Air.local>
2021-07-24 13:10:57 +0800

    Add movies slider in Home page

---
+++ js/slick/slider-settings.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('#movie-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+    mobileFirst: true,
+    pauseOnHover: true,
+    speed: 1000
+  });
+});
